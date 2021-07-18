18bab660a1a8c9a8cf0d6802e32918cbdadba5f0
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-18 06:50:34 -0400

    commit tree 3a6dbe9b2aaf5d929e2d2cb68cdbd1179d8ec5b5

---
+++ app/models/preference.rb
@@ -0,0 +1,11 @@
+class Preference < ActiveRecord::Base
+    
+    # attr_accessor :allow_create_artists, :allow_create_songs
+
+    # def initalize(allow_create_artists:, allow_create_songs:)
+    #     @allow_create_artists = allow_create_artists
+    #     @allow_create_songs = allow_create_songs
+
+    # end
+
+end
